569c8bd67dd22e1f27ff07648fc5203004ce6c0d
jurisukun <abucejojul08@gmail.com>
2023-11-29 15:11:35 +0800

    added functionalities

--- front/src/components/Form.jsx
+++ front/src/components/Form.jsx
@@ -1,7 +1,78 @@
 import React from "react";
 import Map from "./Map.jsx";
 
+import { useState, useRef } from "react";
+
 function Form() {
+  const latitudeRef = useRef(0);
+  const longitudeRef = useRef(0);
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [convertedLatitude, setConvertedLatitude] = useState(null);
+  const [convertedLongitude, setConvertedLongitude] = useState(null);
+  const [savedCoords, showSavedCoords] = useState(false);
+
+  function computeDMS(decimaldegree) {
+    let original = decimaldegree;
+    let Degrees;
+    let Minutes;
+    let Seconds;
+
+    if (decimaldegree < 0) {
+      decimaldegree *= -1;
+    }
+    Degrees = Math.floor(decimaldegree);
+    Minutes = Math.floor((decimaldegree - Degrees) * 60);
+    Seconds = (decimaldegree - Degrees - Minutes / 60) * 3600;
+
+    if (original < 0) {
+      Degrees *= -1;
+    }
+
+    return [Degrees, Minutes, Seconds];
+  }
+
+  function convert() {
+    let latitude = latitudeRef.current.value;
+    let longitude = longitudeRef.current.value;
+    if (
+      !latitude ||
+      !longitude ||
+      latitude > 90 ||
+      longitude > 180 ||
+      latitude < -90 ||
+      longitude < -180
+    )
+      return alert("Invalid coordinate values");
+
+    latitude = Number(latitude);
+    longitude = Number(longitude);
+
+    const [latitudeDegrees, latitudeMinutes, latitudeSeconds] =
+      computeDMS(latitude);
+    const [longitudeDegrees, longitudeMinutes, longitudeSeconds] =
+      computeDMS(longitude);
+
+    setConvertedLatitude(
+      `${latitudeDegrees}° ${latitudeMinutes}' ${latitudeSeconds}'' `
+    );
+    setConvertedLongitude(
+      `${longitudeDegrees}° ${longitudeMinutes}' ${longitudeSeconds}'' `
+    );
+    showSavedCoords(true);
+  }
+
+  function save() {
+    axios
+      .post("http://localhost:3001/api/savecoords", { latitude, longitude })
+      .then((res) => {
+        if (res.status === 200) alert("Coordinates saved successfully");
+      })
+      .catch((err) => {
+        alert("Error saving coordinates");
+        console.log(err);
+      });
+  }
   return (
     <div className="bg-white p-8  shadow-lg shadow-slate-500 rounded-lg min-w-[300px] max-w-[700px] w-[80%] md:flex md:flex-row flex flex-col gap-4">
       <div className="w-full">
@@ -18,7 +89,10 @@ function Form() {
               Latitude
             </label>
             <input
-              defaultValue={0}
+              ref={latitudeRef}
+              onChange={(e) => {
+                setLatitude(Number(e.target.value));
+              }}
               min={-90}
               max={90}
               type="number"
@@ -34,7 +108,7 @@ function Form() {
               id="latitudeDMS"
               className="border rounded px-3 py-2 w-full"
               placeholder="0° 0' 0'' "
-              defaultValue={0}
+              defaultValue={convertedLatitude}
             />
           </div>
         </div>
@@ -48,7 +122,10 @@ function Form() {
               Longitude
             </label>
             <input
-              defaultValue={0}
+              ref={longitudeRef}
+              onChange={(e) => {
+                setLongitude(Number(e.target.value));
+              }}
               min={-180}
               max={180}
               type="number"
@@ -64,17 +141,29 @@ function Form() {
               id="longitudeDMS"
               className="border rounded px-3 py-2 w-full"
               placeholder="0° 0' 0'' "
+              defaultValue={convertedLongitude}
             />
           </div>
         </div>
         <div className="flex flex-row justify-center items-center gap-4 w-full">
           <div className="w-[40%] flex items-center justify-center">
-            <button className=" border-0 bg-green-500  text-white rounded p-2 hover:bg-green-400 whitespace-nowrap w-[95%]  md:text-[min(2vw,12px)] text-[10px]">
+            <button
+              onClick={convert}
+              className=" border-0 bg-green-500  text-white rounded p-2 hover:bg-green-400 whitespace-nowrap w-[95%]  md:text-[min(2vw,12px)] text-[10px]"
+            >
               Convert Coords
             </button>
           </div>
           <div className="w-[40%] flex items-center justify-center">
-            <button>Save Coords</button>
+            <button
+              disabled={!savedCoords}
+              onClick={save}
+              className={`border-0  text-white p-2 rounded hover:bg-blue-400 whitespace-nowrap  w-[95%] max-w-[150px] md:text-[min(2vw,12px)]  text-[10px] ${
+                savedCoords ? "bg-blue-500" : "bg-blue-400"
+              }`}
+            >
+              Save Coords
+            </button>
           </div>
         </div>
       </div>
--- front/src/components/Map.jsx
+++ front/src/components/Map.jsx
@@ -7,7 +7,7 @@ function Map() {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d451891.1108741885!2d-82.90095463001057!3d27.76666814852018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c31f1d2a58a99d%3A0x305de1e1b5f9776d!2sTampa%20Bay!5e0!3m2!1sen!2sph!4v1701239405317!5m2!1sen!2sph"
         width="100%"
         height="100%"
-        allowfullscreen=""
+        allowFullScreen={true}
         loading="lazy"
       ></iframe>
     </div>
